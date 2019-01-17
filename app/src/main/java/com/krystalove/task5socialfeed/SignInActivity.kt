bf8cd76ba6fe2a4f55dd6e96e9095fab9af7ebce
Денис <denis.zhuravkov@gmail.com>
2019-01-17 18:26:59 +0200

    - Login Display

---
+++ app/src/main/java/com/krystalove/task5socialfeed/SignInActivity.kt
@@ -0,0 +1,48 @@
+package com.krystalove.task5socialfeed
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    private val USER_LOGIN = 1
+    private val USER_ERROR = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        sign_in_button.setOnClickListener { login() }
+    }
+
+    private fun tryLogin(): Int {
+        val password = password_field.text.toString()
+        val login = login_field.text.toString()
+        val message =
+            if (login.isEmpty() && password.isEmpty()) R.string.warning_login_and_password
+            else if (login.isEmpty()) R.string.warning_login
+            else if (password.isEmpty()) R.string.warning_password
+            else if (login != TestUser.Login || password != TestUser.Password) R.string.warning_incorrect
+            else null
+        if (message is Int) {
+            Snackbar.make(root_layout, message, Snackbar.LENGTH_SHORT).show()
+            return USER_ERROR
+        }
+        return USER_LOGIN
+
+    }
+
+    private fun login() {
+        if (tryLogin() == USER_LOGIN) {
+
+        }
+    }
+
+    private object TestUser {
+        const val Login = "test"
+        const val Password = "test"
+    }
+}
+
