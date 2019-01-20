8db437dc67ed763795932d63e6801b8e76db1411
krystaLove <denis.zhuravkov@gmail.com>
2019-01-20 23:51:16 +0200

    - AndroidX Libraries

--- app/src/main/java/com/krystalove/task5socialfeed/SignInActivity.kt
+++ app/src/main/java/com/krystalove/task5socialfeed/SignInActivity.kt
@@ -1,9 +1,9 @@
 package com.krystalove.task5socialfeed
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
